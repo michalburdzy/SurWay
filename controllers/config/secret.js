fae11b49decad31f3b805793187174eb967b7347
michal <mmicalt@gmail.com>
2019-01-29 23:09:43 +0100

    fix: removed vulnerable secret file

--- controllers/config/secret.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mongoURI:
-    'mongodb://adminprod:password1234prod@ds141783.mlab.com:41783/survay-prod',
-  clientID:
-    '145327581965-6cabi6tvvighm9lap76noit5a13fp27h.apps.googleusercontent.com',
-  clientSecret: 'lqULlSznQUhXwL9lZifBZqzV',
-  cookieKey:
-    'asdkasdnwedrfghjkliuytfdcvbnhtrdfgertyujhgfde3456uhutrfy5456yhgfder4tghuyt',
-};
