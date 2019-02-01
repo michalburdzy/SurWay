a69dec249b81cfa11d45072d994330bcfa2dea4f
michal <mmicalt@gmail.com>
2019-02-01 20:58:32 +0100

    chore: wiring CI

---
+++ controllers/config/ci.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mongoURI: '127.0.0.1:2708/surway',
+  clientID:
+    '145327581965-6cabi6tvvighm9lap76noit5a13fp27h.apps.googleusercontent.com',
+  // '424950884378-l7jqt3fjb7aal3rd7o6j3edisv90tp91.apps.googleusercontent.com',
+  clientSecret: 'lqULlSznQUhXwL9lZifBZqzV',
+  // clientSecret: 'yaHnG-hvVlBq9slbgslV7uTq',
+  cookieKey:
+    'asdkasdnwedrfghjkliuytfdcvbnhtrdfgertyujhgfde3456uhutrfy5456yhgfder4tghuyt',
+  stripePublishableKey: 'pk_test_Nfn3yLsmuboOHAwZNh3MBhbH',
+  stripeSecretKey: 'sk_test_kGPbrp6WNAho3xc9viLAIHlf',
+  sendgridKey:
+    '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',
+  emailDomain: 'http://localhost:3000',
+  testUserId: '5c25ae528a4ef616b8db1082'
+};
